5298e8330248a15e0b2e2f3518240227794d02d5
Sae Hun Kim <kimsaehun@csu.fullerton.edu>
2016-11-25 21:42:42 -0800

    Select color using button.

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,57 @@
 window.onload = function() {
-  // grab color blocks
-  var colorBlocks = document.querySelectorAll(".color-block");
-
   // color schema
   var schema = new Vue({
-    el: '#schema',
+    el: '#schema-wrapper',
     data: {
-      colorBlocks: colorBlocks,
       activeColorBlock: null
+    },
+    computed: {
+      colorBlocks: function() {
+        return Array.prototype.slice.call(document.querySelectorAll(".color-block"));
+      },
+      numColorBlocks: function() {
+        return this.colorBlocks.length;
+      }
     },
     methods: {
       selectColor: function(event) {
-        // deselect the current active color
         if (this.activeColorBlock != null) {
+          // deselect the current active color
           this.activeColorBlock.classList.remove('active');
         }
         // select the color
-        event.target.classList.add('active');
         this.activeColorBlock = event.target;
+        this.activeColorBlock.classList.add('active');
+      },
+      selectAbove: function() {
+        // default selection
+        if (this.activeColorBlock == null) {
+          this.activeColorBlock = this.colorBlocks[0];
+        }
+        else {
+          // deselect the current active color
+          this.activeColorBlock.classList.remove('active');
+          // get index of active color
+          var activeIndex = this.colorBlocks.indexOf(this.activeColorBlock);
+          // select the color block above
+          this.activeColorBlock = this.colorBlocks[(activeIndex - 1 + this.numColorBlocks) % this.numColorBlocks ];
+        }
+        this.activeColorBlock.classList.add('active');
+      },
+      selectBelow: function() {
+        // default selection
+        if (this.activeColorBlock == null) {
+          this.activeColorBlock = this.colorBlocks[0];
+        }
+        else {
+          // deselect the current active color
+          this.activeColorBlock.classList.remove('active');
+          // get index of active color
+          var activeIndex = this.colorBlocks.indexOf(this.activeColorBlock);
+          // select the color block below
+          this.activeColorBlock = this.colorBlocks[(activeIndex + 1) % this.numColorBlocks ];
+        }
+        this.activeColorBlock.classList.add('active');
       }
     }
   });
